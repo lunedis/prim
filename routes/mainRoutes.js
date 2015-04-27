9adeac743008d03fc953e0fbaebf72217407584d
Kalu <info@zap.de.com>
2015-04-27 09:04:14 +0200

    fastRender test

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,59 +1,11 @@
 Router.route('/', {
+	name: 'home',
 	action: function() {
 		this.render('home');
-  		SEO.set({ title: 'Home -' + Meteor.App.NAME });
-	},
-	fastRender: true
-});
-
-Router.route('fittings', {
-	action: function() {
-		this.render('fittings');
-		SEO.set({ title: 'Fittings - ' + Meteor.App.NAME });
+  		SEO.set({ title: 'Home - ' + Meteor.App.NAME });
 	},
 	waitOn: function() {
-		return Meteor.subscribe('fittings');
-	},
-	fastRender: true
-});
-
-Router.route('fittings/edit/:_id', {
-	action: function() {
-		this.render('fittings-edit', {
-			data: function() {
-				return {fit: Fittings.findOne({_id: this.params._id})};
-			}
-		});
-		SEO.set({title: 'Edit Fitting - ' + Meteor.App.NAME});
-	},
-	waitOn: function() {
-		return Meteor.subscribe('fit', this.params._id);
-	},
-	fastRender: true
-});
-
-Router.route('warlords', {
-	action: function() {
-		this.render('warlords');
-		SEO.set({ title: 'Warlords - ' + Meteor.App.NAME });
-	},
-	waitOn: function() {
-		return Meteor.subscribe('warlords');
-	},
-	fastRender: true
-});
-
-Router.route('warlords/edit/:_id', {
-	action: function() {
-		this.render('warlords-edit', {
-			data: function() {
-				return {warlord: Warlords.findOne({_id: this.params._id})};
-			}
-		});
-		SEO.set({title: 'Edit Warlord - ' + Meteor.App.NAME});
-	},
-	waitOn: function() {
-		return Meteor.subscribe('warlord', this.params._id);
+		return [Meteor.subscribe('fittings'), Meteor.subscribe('warlords')];
 	},
 	fastRender: true
 });
