378c4eda1c0823504893172c6a725421cfa8c7c8
Kalu <info@zap.de.com>
2015-04-27 09:05:07 +0200

    forgot to add new route files

---
+++ routes/fittingsRoutes.js
@@ -0,0 +1,25 @@
+Router.route('fittings', {
+	action: function() {
+		this.render('fittings');
+		SEO.set({ title: 'Fittings - ' + Meteor.App.NAME });
+	},
+	waitOn: function() {
+		return [Meteor.subscribe('fittings'), Meteor.subscribe('warlords')];
+	},
+	fastRender: true
+});
+
+Router.route('fittings/edit/:_id', {
+	action: function() {
+		this.render('fittings-edit', {
+			data: function() {
+				return {fit: Fittings.findOne({_id: this.params._id})};
+			}
+		});
+		SEO.set({title: 'Edit Fitting - ' + Meteor.App.NAME});
+	},
+	waitOn: function() {
+		return Meteor.subscribe('fit', this.params._id);
+	},
+	fastRender: true
+});
---
+++ routes/warlordsRoutes.js
@@ -0,0 +1,25 @@
+Router.route('warlords', {
+	action: function() {
+		this.render('warlords');
+		SEO.set({ title: 'Warlords - ' + Meteor.App.NAME });
+	},
+	waitOn: function() {
+		return [Meteor.subscribe('fittings'), Meteor.subscribe('warlords')];
+	},
+	fastRender: true,
+});
+
+Router.route('warlords/edit/:_id', {
+	action: function() {
+		this.render('warlords-edit', {
+			data: function() {
+				return {warlord: Warlords.findOne({_id: this.params._id})};
+			}
+		});
+		SEO.set({title: 'Edit Warlord - ' + Meteor.App.NAME});
+	},
+	waitOn: function() {
+		return Meteor.subscribe('warlord', this.params._id);
+	},
+	fastRender: true
+});
